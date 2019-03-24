5ee83f3e44e93d851a089f4a8a9bc4d1b986ab47
BermetZ <zhumakadyrkyzy.b@gmail.com>
2019-03-24 17:04:37 +0600

    Initial commit.

---
+++ counting_valleys.py
@@ -0,0 +1,14 @@
+def counting_valleys():
+    s = list(input("Enter Gary's route: "))
+    n = len(s)
+    valleys = 0
+    sea_level = 0
+    for step in s:
+        if step == 'U':
+            sea_level += 1
+            if sea_level == 0:
+                valleys += 1
+        elif step == 'D':
+            sea_level -= 1
+    print('Gary took {} steps and was in {} valleys.'.format(n, valleys))
+counting_valleys()
